ee913884b5a06c423c2be4fcdaa7346f375e542b
Fachtna Roe <fachtna@fachtnaroe.ie>
2014-04-19 12:51:23 +0100

    Integrate new graphics, add depth to text layers.

--- perlShips.pl
+++ perlShips.pl
@@ -42,16 +42,23 @@ use SDL::Mouse;
 use SDLx::Text;
 
 #use constant => ;
-use constant screenWidth => 660;
-use constant screenHeight => 494;
-use constant textPromptX => 15;
-use constant textPromptY => 415;
-use constant rectPromptBoxX => 11;
-use constant rectPromptBoxY => 408;
+use constant screenWidth => 802;
+use constant screenHeight => 600;
+use constant textPromptX => 50;
+use constant textPromptY => 480;
+use constant rectPromptBoxX => 44;
+use constant rectPromptBoxY => 477;
 my $lineHeight=20;
+
+use constant tableTopLeftX => 42; 
+use constant tableTopLeftY => 123;
+use constant cellWidth => 32;
+use constant cellHeight => 32;
+use constant secondTableOffset => 320;
 
 use constant verbFire => "fire"; 
 use constant verbQuit => "quit";
+use constant verbHelp => "help";
 
 my ($gui, $event, $exiting );
 # First create a new App
@@ -69,7 +76,7 @@ $gui->add_event_handler( \&quit_event );
 #  "Can't call method "w" on an undefined value at ThisFile.pl line XX."
 # will be received.
 my ($background, $rectBackground);
-$background = SDL::Image::load('graphics/bg_ocean_moon_actual.png');
+$background = SDL::Image::load('graphics/Battleship_Main_Image.png');
 # Create a rectangle for the background image
 $rectBackground = SDL::Rect->new(0,0,
     $background->w,
@@ -78,7 +85,7 @@ $rectBackground = SDL::Rect->new(0,0,
 SDL::Video::blit_surface($background, $rectBackground, $gui, $rectBackground );
 
 my ($promptBox, $rectPromptBox, @textPrompt, $rectPromptBoxMaster);
-$promptBox = SDL::Image::load('graphics/promptBox_moon.png');
+$promptBox = SDL::Image::load('graphics/promptBox_moon2.png');
 $rectPromptBoxMaster=SDL::Rect->new(0,0, $promptBox->w,$promptBox->h);
 # Create a rectangle for the background image
 $rectPromptBox = SDL::Rect->new(rectPromptBoxX, rectPromptBoxY,
@@ -89,17 +96,22 @@ SDL::Video::blit_surface($promptBox, $rectPromptBoxMaster, $gui, $rectPromptBox 
 # Add in text box/locations; we'll put text in it later:
 # 3 lines
 $textPrompt[0] = SDLx::Text->new(size=>'18', # font can also be specified
-                            color=>[255,255,255], # [R,G,B]
+                            color=>[255,171,0], # [R,G,B]
                             x => textPromptX,
                             y => textPromptY);
 $textPrompt[1] = SDLx::Text->new(size=>'18', # font can also be specified
-                            color=>[255,255,255], # [R,G,B]
+                            color=>[255,171,0], # [R,G,B]
                             x => textPromptX,
                             y => textPromptY+$lineHeight);
 $textPrompt[2] = SDLx::Text->new(size=>'18', # font can also be specified
-                            color=>[255,255,255], # [R,G,B]
+                            color=>[255,100,0], # [R,G,B]
                             x => textPromptX,
                             y => textPromptY+($lineHeight*2));
+
+my ($imageHit, $rectImageHit, $rectImageHitMaster);
+$imageHit = SDL::Image::load('graphics/hit_2.png');
+$rectImageHitMaster=SDL::Rect->new(0,0, $imageHit->w,$imageHit->h);
+
 
 # Create a new event structure variable
 $event = SDL::Event->new();
@@ -117,13 +129,14 @@ while ( !$exiting ) {
   $gui->update;
   getPlayerMove();
   testHits();
+  showBoard();
   getComputerMove();
   testHits();
   # Update the queue to recent events
   SDL::Events::pump_events();
   # process all available events
   while (SDL::Events::poll_event($event)) {
-    # check by Event type      
+    # check by Event type
     if ($event->type == SDL_QUIT) {
       &quit_event(); 
     }
@@ -138,15 +151,6 @@ while ( !$exiting ) {
   # slow things down if required
   $gui->delay(100);
 } # game loop
-
-#while (NOT ending) {
-
-  getPlayerMove();
-  testHits();
-  #checkgameend
-  makeComputerMove();
-  #checkgameend
-#}
 
 sub quit_event {
 	exit;
@@ -201,72 +205,24 @@ my $playerships= $carrier, $battleship, $destroyer, $frigate, $submarine;
 
 }
 
-
-
-# the sub gets the places move
-sub getPlayerMove {
-
-  # get the players move.
-  #$textPrompt->write_to($gui,"Player Location: ()");
-}
-
-
-
-# the sub gets the computer move
-sub makeComputerMove {
-
-
-# make the computer fire one shot
-#do 
-
-# until the computer has used his 1 shot.
-# until
-
-
-}
-
-
-
 # this sub gets the players move and see if or the ai has hit anything if the have hit something or missed this sub will display it.
 sub testHits {
-
 # announce if the player and AI have either hit a ship or have missed.
 
-
 }
-
-
 
 sub winner {
 
 # if the player or ai loses all his ships then the person who still has atleasts 1 health point left wins
 
-
 }
 
 # testing purposes, place a destroyer
 # note this is [row][column] positioning
-$oceanPlayer[1][2]="d";
-$oceanPlayer[5][5]="c";
-$oceanPlayer[9][9]="b";
-$oceanPlayer[6][6]="f";
-$oceanPlayer[7][8]="s";
-
-
-# testing purposes, display the player's ocean
-foreach (my $x=0;$x<=size;$x++) {
-  foreach (my $y=0;$y<=size;$y++) {
-    print $oceanPlayer[$x][$y];
-  }
-  # after each line, CR
-  print "\n";
-}
+#$oceanPlayer[1][2]="d";
 
 # demonstrate ability to test values in the ocean,
 #if ($oceanPlayer[1][2] eq "d") {
-
-
-##############################################################################################################################################################
 
 sub getPlayerMove {
   promptClear();
@@ -300,11 +256,7 @@ sub prompt {
 }
 
 sub getPlayerPositions {
-  prompt(0, "Your move:");
-  my $pos=getCommand();
-  showBoard();
-  prompt(1, "Got: $pos");
-  print "Got $pos";
+
 }
 
 sub getCommand {
@@ -351,9 +303,13 @@ sub translateKey {
 sub showBoard {
 # this brings up the battlefield where the ships will be placed it is a 10 by 10 grid with a background image infront
 #$getboard = 'battleship_array.pl' 'battleship_imagebackground';
-  foreach (my $x=0;$x<=size;$x++) {
-    foreach (my $y=0;$y<=size;$y++) {
-      print $oceanPlayer[$x][$y];
+  my ($cols, $rows);
+  foreach ($cols=0;$cols<size;$cols++) {
+    foreach ($rows=0;$rows<size;$rows++) {
+      #print $oceanPlayer[$cols][$rows];
+      my $x = tableTopLeftX + ($rows * cellWidth);
+      my $y = tableTopLeftY + ($cols * cellHeight);
+      drawHit($x, $y);
     }
     # after each line, CR
     print "\n";
@@ -370,21 +326,55 @@ sub setComputerPositions {
 
 sub processCommand {
   my $commandString=shift; # get what's passed in
-  my @command=split "\s", $commandString; # split by whitespace into words
+  my @command=split '\s', $commandString; # split by whitespace into words
   my $verb=$command[0];
   if ($verb eq verbFire) {
-    unshift @command; # get rid of verb now it's identified
+    shift @command; # get rid of verb now it's identified
     commandFire(@command);
   }
   elsif ($verb eq verbQuit) {
     $exiting=1;
   }
+  elsif ($verb eq verbHelp) {
+    prompt(2, "Sorry. Help not implemented yet.");
+    pause();
+
+  }
   else {
-    prompt(2, "Unknown command ($verb) - press any key to continue");
-    $gui->pause();
+    prompt(2, "Unknown command ($verb)");
+    pause();
   }
 }
 
 sub commandFire {
-
+  my @command=shift;
+  my $target=shift @command; # get grid target 
+  my ($gridLetter, $gridNumber) = split '', $target; # split into characters
+  if ( (($gridLetter ge 'a') && ($gridLetter le 'j'))  # is letter in ok range
+     && (($gridNumber >= 0) && ($gridLetter <= 9)) ) { # is number in ok range
+    print "[$gridLetter][$gridNumber]\n";
+  }
+  else {
+    prompt (2, "Unknown grid reference");
+    pause();
+  }
 }
+
+sub pause {
+  my $time=shift;
+  if ($time > 0) {
+    sleep ($time);
+  }
+  else {
+    sleep (1);
+  }
+}
+sub drawHit {
+  my ($x, $y) = @_;
+  #my $y = shift;
+  my $rectImageHit = SDL::Rect->new( $x, $y, $imageHit->w, $imageHit->h );
+  SDL::Video::blit_surface($imageHit, $rectImageHitMaster, $gui, $rectImageHit );
+  SDL::Video::update_rects($gui, $rectImageHit);
+}
+
+
